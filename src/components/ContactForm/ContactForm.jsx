697e386bcfdf8692a1a2f1528d9a1557dbd2ee46
krok-code <maks.smolinskyi@gmail.com>
2024-01-02 00:28:07 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,72 +1,58 @@
-import { Formik } from 'formik';
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import { Form, Label, Input, Button } from './ContactForm.styled';
 
-// export const ContactForm = ({ onSubmit }) => {
-//   const handleSubmit = (values, actions) => {
-//     const contact = {
-//       id: nanoid(6),
-//       ...values,
-//     };
-//     onSubmit(contact);
-//     actions.resetForm();
-//   };
+export class Phonebook extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
 
-handleChange = e => {
+  handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
-    this.props.handleSubmit(this.state);
-    form.reset();
+    this.props.onSubmit(this.state.name, this.state.number);
+    this.setState({ name: '', number: '' });
   };
 
   render() {
+    const { name, number } = this.state;
     return (
-      <Formik
-        initialValues={{ name: '', number: '' }}
-        onSubmit={this.handleSubmit}
-      >
-        {props => {
-          return (
-            <form>
-              <div>
-               <div>
-                 <label htmlFor="name">Name</label>
-                 <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    placeholder="Enter your"
-                    value={props.values.name}
-                    onChange={props.handleChange}
-                  />
-                </div>
-                <label>
-                  <label htmlFor="number">Number</label>
-                  <input
-                    id="number"
-                    type="tel"
-                    name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    placeholder="+38 000 000 00 00"
-                    value={props.values.number}
-                    onChange={props.handleChange}
-                  />
-                </label>
-              </div>
-<button type="submit">Add contact</button>
-            </form>
-          );
-        }}
-      </Formik>
+      <Form onSubmit={this.handleSubmit}>
+        <Label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </Label>
+        <Label>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
     );
   }
+}
+
+Phonebook.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 10px;
+  transition: 0.5s;
+  outline: none;
+
+  &:hover {
+    background-color: #d9dae0;
+  }
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  padding: 10px 20px;
+  margin: 0px 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgb(33, 150, 243);
+  color: rgb(255, 255, 255);
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1.2;
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px;
+
+  &:hover,
+  &:focus {
+    background-color: #fff;
+    color: #2196f3;
+  }
+
+  &:active {
+    scale: 0.95;
+  }
+`;
