98e76081d8f16646f1049316c42ee2e6f7b54cb9
krok-code <maks.smolinskyi@gmail.com>
2024-01-02 00:32:43 +0200

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,31 @@
+import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { List, ListItem, ItemWrapper, Button } from './ContactList.styled';
 
-export const ContactList = ({ contacts = [], deleteContact }) => (
-  <div>
-    <ul>
-      {contacts.map((contact, id) => (
-        <ListItem key={id}>
-          <ItemWrapper>
-            <p>{contact.name}: </p>
-            <p>{contact.number}</p>
-          </ItemWrapper>
-          <button type="button" onClick={() => deleteContact(contact.id)}>
-            <FaTrashAlt
-              style={{
-                cursor: 'pointer',
-                color: 'crimson',
-              }}
-            />
-          </button>
-        </ListItem>
-      ))}
-    </ul>
-  </div>
+export const ContactList = ({ contacts, onDeleteContact }) => (
+  <List>
+    {contacts.map(({ id, name, number }) => (
+      <ListItem key={id}>
+        <ItemWrapper>
+          <p>{name}: </p>
+          <p>{number}</p>
+        </ItemWrapper>
+        <Button type="button" onClick={() => onDeleteContact(id)}>
+          <FaTrashAlt />
+        </Button>
+      </ListItem>
+    ))}
+  </List>
 );
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -9,17 +9,61 @@ export const List = styled.ul`
   margin: 0;
 `;
 
-export const ListWrapper = styled.div`
+export const ListItem = styled.li`
   display: flex;
-  flex-direction: column;
+
+  justify-content: space-between;
   align-items: center;
   width: 100%;
-  padding: 0;
-  margin: 0;
+  padding: 0 0 0 20px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 5px;
+
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 10px;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: #d9dae0;
+  }
 `;
 
 export const ItemWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: flex-start;
+
+  p {
+    margin: 0;
+    padding: 10px 0;
+  }
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  padding: 5px 25px;
+  margin: 0px 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgb(33, 150, 243);
+  color: rgb(255, 255, 255);
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1.2;
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px;
+
+  &:hover,
+  &:focus {
+    background-color: #fff;
+    color: #2196f3;
+  }
+
+  &:active {
+    scale: 0.95;
+  }
 `;
