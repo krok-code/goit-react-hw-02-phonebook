a6980e948936a7102f21818fb1bd68f8826844e0
krok-code <maks.smolinskyi@gmail.com>
2024-01-01 13:20:47 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
 import { Component } from 'react';
 import { GlobalStyle } from 'GlobalStyle';
-import { Container } from './App.styled';
 import { Section } from './Section/Section';
+import { Container } from './App.styled';
 
 export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    ],
+    name: '',
+  };
+
   render() {
     return (
       <Container>
-        <GlobalStyle />
         <Section title="Phone book"></Section>
+        <GlobalStyle />
         <Section title="Contacts"></Section>
       </Container>
     );
