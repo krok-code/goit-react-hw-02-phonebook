785dd97ab28c85494b188a3bb8ca3b5dbe204e56
krok-code <maks.smolinskyi@gmail.com>
2024-01-01 23:56:32 +0200

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,21 @@
-// import { ContactItem } from 'components/ContactItem/ContactItem';
+import { FaTrashAlt } from 'react-icons/fa';
+
+export const ContactList = ({ contacts = [], deleteContact }) => (
+  <div>
+    <ul>
+      {contacts.map((contact, id) => (
+        <li key={id}>
+          {contact.name}: {contact.number}
+          <button type="button" onClick={() => deleteContact(contact.id)}>
+            <FaTrashAlt
+              style={{
+                cursor: 'pointer',
+                color: 'crimson',
+              }}
+            />
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
