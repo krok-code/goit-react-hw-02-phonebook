8addafd948cea02d7fa27499dc4d5289152a43d0
krok-code <maks.smolinskyi@gmail.com>
2024-01-07 22:53:30 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,40 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { GlobalStyle } from 'GlobalStyle';
-import { Section } from './Section/Section';
-import { Container } from './App.styled';
-import { Phonebook } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './ContactFilter/ContactFilter';
+import React, { useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import ContactFilter from './ContactFilter/ContactFilter';
+import { Container, Title, Heading } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    setContacts([...contacts, newContact]);
   };
 
-  addContact = (name, number) => {
-    const { contacts } = this.state;
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    const checkName = contacts.find(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (checkName) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-
-    this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+  const deleteContact = id => {
+    const updateContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updateContacts);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  render() {
-    const filteredContacts = this.getFilteredContacts();
-    return (
-      <Container>
-        <GlobalStyle />
-        <Section title="Phonebook">
-          <Phonebook onSubmit={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter value={this.filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={filteredContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Title>Phonebook📱</Title>
+      <ContactForm addContact={addContact} contacts={contacts} />
+      <Heading>Contacts</Heading>
+      <ContactFilter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+    </Container>
+  );
 }
+
+export default App;
--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  width: 100%;
-  background-color: #d9dae0;
-  padding: 20px;
-  margin: 0 auto;
-  transition: 1.5s;
-  overflow-x: hidden;
-  color: #000;
-  background-color: #d9dae0;
+  height: 100vh;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  margin-top: 20px;
+`;
+
+export const Heading = styled.h2`
+  /* display: block; */
 `;
